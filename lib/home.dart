b8aad369172b0aec7246b22e3d85a1b8532badad
MohamedAbdelghanyy <45740410+MohamedAbdelghanyy@users.noreply.github.com>
2020-12-28 22:31:34 +0200

    Wifi Scanning Base

--- lib/home.dart
+++ lib/home.dart
@@ -1,27 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:wifi_flutter/wifi_flutter.dart';
 
-class HomePage extends StatelessWidget {
+void main() => runApp(HomePage());
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Widget> _platformVersion = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: ListView(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              Text(
-                "Home",
-                style: TextStyle(fontSize: 30),
-              ),
-            ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: ListView.builder(
+            itemBuilder: (context, i) => _platformVersion[i],
+            itemCount: _platformVersion.length,
           ),
-        ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final noPermissions = await WifiFlutter.promptPermissions();
+            if (noPermissions) {
+              return;
+            }
+            final networks = await WifiFlutter.wifiNetworks;
+            setState(() {
+              _platformVersion = networks
+                  .map((network) => Text(
+                      "Ssid ${network.ssid} - Strength ${network.rssi} - Secure ${network.isSecure}"))
+                  .toList();
+            });
+          },
+        ),
       ),
     );
   }
