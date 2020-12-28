4f878703f30f0244a05136a70e7f729ff2d46d58
MohamedAbdelghanyy <45740410+MohamedAbdelghanyy@users.noreply.github.com>
2020-12-28 17:46:58 +0200

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,15 +1,8 @@
-<<<<<<< Updated upstream
-import 'package:SafeMove/services/perference_manager.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import './services/auth.dart';
-=======
 import 'package:SafeMove/profile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import './services/auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
->>>>>>> Stashed changes
 
 class LoginForm extends StatefulWidget {
   @override
@@ -18,102 +11,10 @@ class LoginForm extends StatefulWidget {
   }
 }
 
-<<<<<<< Updated upstream
-class _MyHomePageState extends State<MyHomePage> {
-  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-
-  var authHandler = new Auth();
-
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-
-  @override
-  void dispose() {
-    emailController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final emailField = TextField(
-      controller: emailController,
-      obscureText: false,
-      style: style,
-      decoration: InputDecoration(
-          filled: true,
-          fillColor: Colors.white,
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Email",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final passwordField = TextField(
-      controller: passwordController,
-      obscureText: true,
-      style: style,
-      decoration: InputDecoration(
-          filled: true,
-          fillColor: Colors.white,
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Password",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final loginButon = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xffdb1f2c),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {
-          var email = emailController.text;
-          var password = passwordController.text;
-          if (_formKey.currentState.validate()) {
-            authHandler
-                .handleSignInEmail(
-                    emailController.text, passwordController.text)
-                .then((FirebaseUser user) {
-              Navigator.push(context, new MaterialPageRoute(
-                builder: (context) {
-                  PrefManager myPrefManager = new PrefManager();
-                  myPrefManager.setEmail(email);
-                  myPrefManager.setPassword(password);
-                  myPrefManager.setPermission(3);
-                  myPrefManager.saveDataToLocalMemory();
-                  return new TabsScreen();
-                },
-              ));
-            }).catchError((e) {
-              print(e);
-              if (e.toString().contains("ERROR_USER_NOT_FOUND") ||
-                  e.toString().contains("ERROR_WRONG_PASSWORD")) {
-                Fluttertoast.showToast(
-                  msg: "Invalid email or password",
-                  toastLength: Toast.LENGTH_SHORT,
-                );
-              } else {
-                Fluttertoast.showToast(
-                  msg: "Please check your internet connection",
-                  toastLength: Toast.LENGTH_SHORT,
-                );
-              }
-            });
-          }
-        },
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-=======
 class LoginFormState extends State<LoginForm> {
   final _formKey = GlobalKey<FormState>();
   static const routeName = '/login-screen';
   var authHandler = new Auth();
->>>>>>> Stashed changes
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
