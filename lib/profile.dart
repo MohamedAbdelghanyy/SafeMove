673a5ad10a37016ef584b8d7e19ed5624f764c64
AhmedHassanMeso <ahmedmohamedhassan@live.com>
2020-12-28 17:29:44 +0200

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -5,55 +5,50 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 10,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Profile",
+              style: TextStyle(fontSize: 25),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: AssetImage('assets/profile.jpg'), fit: BoxFit.fill),
               ),
-              Text(
-                "Profile",
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: AssetImage('assets/profile.jpg'),
-                      fit: BoxFit.fill),
-                ),
-              ),
-              Table(
-                children: [
-                  TableRow(
-                    children: [
-                      Text("First Name"),
-                      Text("Ahmed"),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      Text("Last Name:"),
-                      Text("Mohamed"),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      Text("Age"),
-                      Text("22"),
-                    ],
-                  ),
-                ],
-              )
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text("First Name: Ahmed"),
+            SizedBox(
+              height: 15,
+            ),
+            Text("Last Name: Mohamed"),
+            SizedBox(
+              height: 15,
+            ),
+            Text("Age: 22"),
+            SizedBox(
+              height: 15,
+            ),
+            Text("Gender: Male"),
+            SizedBox(
+              height: 15,
+            ),
+            Text("Ahmed1705293@miuegypt.edu.eg"),
+          ],
         )
       ],
     );
