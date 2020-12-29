6ddbe8213620cddaa6a44962f2fdb3cfb5101f84
MohamedAbdelghanyy <45740410+MohamedAbdelghanyy@users.noreply.github.com>
2020-12-29 07:55:16 +0200

    Update

--- lib/rooms_screen.dart
+++ lib/rooms_screen.dart
@@ -12,30 +12,78 @@ class RoomsScreen extends StatefulWidget {
 
 class _RoomsScreenState extends State<RoomsScreen> {
   var roomsData = "";
+  Map<String, String> _recResult;
   List<ListTile> roomsDataList = List();
   final dbRef = FirebaseDatabase.instance.reference();
 
-  Future<void> startReading() async {
+  void checkDB() {
+    startReceiving();
+  }
+
+  Future<void> startReceiving() async {
     while (true) {
-      dbRef.reference().child('counter').once().then((DataSnapshot snapshot) {
+      print("rescanning...");
+      _recResult = await loadData();
+      setState(() {});
+      sleep(Duration(seconds: 5));
+    }
+  }
+
+  Future<Map<String, String>> loadData() async {
+    print("Loading wifi list");
+    Map<String, String> htResultNetwork;
+    try {
+      dbRef
+          .reference()
+          .child("data")
+          .child("locations")
+          .child("MIU")
+          .child("rooms")
+          .once()
+          .then((DataSnapshot snapshot) {
         print('Connected to second database and read ${snapshot.value}');
+
+        htResultNetwork = snapshot.value;
+      });
+    } on Exception {
+      htResultNetwork = null;
+    }
+
+    return htResultNetwork;
+  }
+
+  Widget getWidgets() {
+    if (_recResult != null && _recResult.length > 0) {
+      List<ListTile> wifiWList = List();
+
+      _recResult.forEach((k, v) {
         setState(() {
-          roomsData = snapshot.value;
+          wifiWList.add(
+            ListTile(
+              title: Text(k + " - " + v),
+            ),
+          );
         });
       });
-      sleep(Duration(seconds: 5));
+
+      return ListView(
+        padding: kMaterialListPadding,
+        children: wifiWList,
+      );
+    } else {
+      return Text("Checking WiFi!");
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    startReading();
+    checkDB();
     return Scaffold(
       appBar: AppBar(
         title: Text("Rooms"),
       ),
       drawer: MainDrawer(),
-      body: Center(child: Text(roomsData)),
+      body: getWidgets(),
     );
   }
 }
