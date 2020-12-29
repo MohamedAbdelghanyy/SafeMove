29b3b1591057473bdd4a1d5707bc664585b347c9
MohamedAbdelghanyy <45740410+MohamedAbdelghanyy@users.noreply.github.com>
2020-12-29 04:10:27 +0200

    Wi-Fi Screen

--- lib/wifi_screen.dart
+++ lib/wifi_screen.dart
@@ -1,15 +1,80 @@
 import 'package:flutter/material.dart';
-import './main_drawer.dart';
+import 'package:flutter/services.dart';
+import 'package:wifi_iot/wifi_iot.dart';
+import 'dart:io';
 
-class WifiScreen extends StatelessWidget {
+void main() => runApp(WifiScreen());
+
+class WifiScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Wifi"),
+  _WifiScreenState createState() => _WifiScreenState();
+}
+
+class _WifiScreenState extends State<WifiScreen> {
+  List<WifiNetwork> _scanResult;
+
+  void checkWifi() {
+    WiFiForIoTPlugin.isEnabled().then((val) {
+      if (val) {
+        startScanning();
+      } else {
+        print("Wifi is disabled!");
+      }
+    });
+  }
+
+  Future<void> startScanning() async {
+    while (true) {
+      _scanResult = await loadWifiList();
+      setState(() {});
+      sleep(Duration(seconds: 5));
+    }
+  }
+
+  Future<List<WifiNetwork>> loadWifiList() async {
+    List<WifiNetwork> htResultNetwork;
+    try {
+      htResultNetwork = await WiFiForIoTPlugin.loadWifiList();
+    } on PlatformException {
+      htResultNetwork = List<WifiNetwork>();
+    }
+
+    return htResultNetwork;
+  }
+
+  Widget getWidgets() {
+    if (_scanResult != null && _scanResult.length > 0) {
+      List<ListTile> wifiWList = List();
+
+      _scanResult.forEach((wifiNetwork) {
+        setState(() {
+          wifiWList.add(
+            ListTile(
+              title: Text(wifiNetwork.ssid),
+            ),
+          );
+        });
+      });
+
+      return ListView(
+        padding: kMaterialListPadding,
+        children: wifiWList,
+      );
+    } else {
+      return Text("Checking WiFi!");
+    }
+  }
+
+  @override
+  Widget build(BuildContext poContext) {
+    checkWifi();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Wifi Scanner'),
+        ),
+        body: getWidgets(),
       ),
-      drawer: MainDrawer(),
-      body: Text("Placeholder"),
     );
   }
 }
