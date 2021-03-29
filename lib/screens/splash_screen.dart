fbd2de8eebde2f9dd6837f12e4f463de88457c63
MohamedAbdelghanyy <45740410+MohamedAbdelghanyy@users.noreply.github.com>
2021-03-29 23:23:16 +0200

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -71,6 +71,7 @@ class _SplashScreenState extends State<SplashScreen>
 
 import 'package:SafeMove/screens/admin_view.dart';
 import 'package:SafeMove/screens/home.dart';
+import 'package:SafeMove/screens/map_screen.dart';
 import 'package:SafeMove/screens/sign_in.dart';
 import 'package:SafeMove/services/data_manager.dart';
 import 'package:flutter/material.dart';
@@ -101,7 +102,7 @@ class _SplashScreenState extends State<SplashScreen> {
           if (DataManager.mPrefManager.isLoggedIn()) {
             print("Already Logged In");
             if (DataManager.mPrefManager.getType() != 'admin') {
-              return HomeScreen();
+              return MapView();
             } else {
               return AdminView();
             }
