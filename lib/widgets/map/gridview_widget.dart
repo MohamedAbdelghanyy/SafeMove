638672f3cd17bfb1a73b0187cca0ddf8c8534655
Mohamed Abdelghany <mohamedabdelghanyy@gmail.com>
2021-06-10 23:35:56 +0200

    update route painter

--- lib/widgets/map/gridview_widget.dart
+++ lib/widgets/map/gridview_widget.dart
@@ -1,6 +1,5 @@
-import 'package:SafeMove/data/global.dart';
 import 'package:SafeMove/models/floorplan_model.dart';
-import 'package:SafeMove/models/room_model.dart';
+import 'package:SafeMove/widgets/map/route_painter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -69,7 +68,7 @@ class _GridViewWidgetState extends State<GridViewWidget> {
         model.routeData != null
             ? Container(
                 child: CustomPaint(
-                  painter: LinePainter(model.routeData, size.width),
+                  painter: RoutePainter(model.routeData, size.width),
                 ),
               )
             : SizedBox(),
@@ -85,77 +84,5 @@ class _GridViewWidgetState extends State<GridViewWidget> {
         ),
       ],
     );
-  }
-}
-
-class LinePainter extends CustomPainter {
-  RouteData routeData;
-  double cSize;
-  Paint paintt = Paint()
-    ..color = Colors.blue[900]
-    ..strokeWidth = 5
-    ..strokeCap = StrokeCap.round;
-  bool isDrawingStarted = false, isRoomXFound = false, isRoomYFound = false;
-
-  LinePainter(this.routeData, this.cSize);
-
-  @override
-  void paint(final Canvas canvas, final Size size) {
-    /*List xy = [0.0362, -0.256];
-    print('[' + xy[0].toString() + ', ' + xy[1].toString());
-    canvas.drawLine(Offset(cSize * xy[0], cSize * xy[1]),
-        Offset(cSize * xy[0], cSize * xy[1]), paintt);*/
-
-    canvas.drawLine(
-        Offset(cSize * routeData.roomX.doorPosition[0],
-            cSize * routeData.roomX.doorPosition[1]),
-        Offset(cSize * routeData.roomX.corridorPosition[0],
-            cSize * routeData.roomX.corridorPosition[1]),
-        paintt);
-
-    canvas.drawLine(
-        Offset(cSize * routeData.roomY.doorPosition[0],
-            cSize * routeData.roomY.doorPosition[1]),
-        Offset(cSize * routeData.roomY.corridorPosition[0],
-            cSize * routeData.roomY.corridorPosition[1]),
-        paintt);
-
-    for (int i = 0; i < Global.corridorCheckPoint.length; i++) {
-      if (routeData.roomX.corridorPosition[0] ==
-              Global.corridorCheckPoint[i]['position'][0] &&
-          routeData.roomX.corridorPosition[1] ==
-              Global.corridorCheckPoint[i]['position'][1] &&
-          !isRoomXFound) {
-        isRoomXFound = true;
-        isDrawingStarted = true;
-      }
-      if (routeData.roomY.corridorPosition[0] ==
-              Global.corridorCheckPoint[i]['position'][0] &&
-          routeData.roomY.corridorPosition[1] ==
-              Global.corridorCheckPoint[i]['position'][1] &&
-          !isRoomYFound) {
-        isRoomYFound = true;
-        isDrawingStarted = true;
-      }
-      if (!(isRoomXFound && isRoomYFound) &&
-          isDrawingStarted &&
-          (i + 1 < Global.corridorCheckPoint.length)) {
-        canvas.drawLine(
-          Offset(cSize * Global.corridorCheckPoint[i]['position'][0],
-              cSize * Global.corridorCheckPoint[i]['position'][1]),
-          Offset(cSize * Global.corridorCheckPoint[i + 1]['position'][0],
-              cSize * Global.corridorCheckPoint[i + 1]['position'][1]),
-          paintt,
-        );
-      }
-    }
-    isRoomXFound = false;
-    isRoomYFound = false;
-    isDrawingStarted = false;
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
   }
 }
