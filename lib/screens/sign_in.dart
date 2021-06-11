23263f74db4adc47294bac166f70c5cdbb960128
Mohamed Abdelghany <mohamedabdelghanyy@gmail.com>
2021-06-11 05:20:50 +0200

    update sign in

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,7 +1,9 @@
+import 'package:SafeMove/data/global.dart';
 import 'package:SafeMove/screens/sign_up.dart';
 import 'package:SafeMove/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:SafeMove/services/auth.dart';
+import 'package:flutter/rendering.dart';
 
 class SignInScreen extends StatefulWidget {
   static const routeName = '/signin-screen';
@@ -29,251 +31,159 @@ class SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Global.secondaryColor,
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [Color(0xFFff8e89), Color(0xFFff7366)],
-            ),
-          ),
-          child: ListView(
-            children: [
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    //Logo section
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image(
-                          image: AssetImage("assets/covidlogo.png"),
-                          width: 350,
-                          height: 300,
-                        )
-                      ],
-                    ),
-                    //Email textfield
-                    Form(
-                      key: _formKey,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Expanded(
-                                child: Padding(
-                                  padding:
-                                      EdgeInsets.only(left: 20.0, right: 20.0),
-                                  child: Container(
-                                      alignment: Alignment.center,
-                                      height: 60.0,
-                                      decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius:
-                                              BorderRadius.circular(50)),
-                                      child: TextFormField(
-                                          onSaved: (value) {
-                                            _email = value;
-                                          },
-                                          controller: emailController,
-                                          validator: (value) {
-                                            if (value.isEmpty) {
-                                              return 'Please enter your email';
-                                            }
-                                            return null;
-                                          },
-                                          decoration: InputDecoration(
-                                              enabledBorder: OutlineInputBorder(
-                                                  borderSide: BorderSide(
-                                                      color: Colors.grey[500]),
-                                                  borderRadius:
-                                                      BorderRadius.all(
-                                                          Radius.circular(50))),
-                                              focusedBorder: OutlineInputBorder(
-                                                  borderSide: BorderSide(
-                                                      color: Colors.green[300]),
-                                                  borderRadius:
-                                                      BorderRadius.all(
-                                                          Radius.circular(50))),
-                                              prefixIcon: Icon(Icons.email),
-                                              hintText: 'Email'),
-                                          style: TextStyle(
-                                              fontSize: 20.0,
-                                              color: Colors.black))),
-                                ),
-                              )
-                            ],
-                          ),
-                          //Password textfield
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Expanded(
-                                child: Padding(
-                                  padding: EdgeInsets.only(
-                                      left: 20.0, right: 20.0, top: 10.0),
-                                  child: Container(
-                                      alignment: Alignment.center,
-                                      decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius:
-                                              BorderRadius.circular(50)),
-                                      child: TextFormField(
-                                        onSaved: (value) {
-                                          _password = value;
-                                        },
-                                        controller: passwordController,
-                                        validator: (value) {
-                                          if (value.isEmpty) {
-                                            return 'Please enter your password';
-                                          }
-                                          return null;
-                                        },
-                                        decoration: InputDecoration(
-                                            enabledBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: Colors.grey[500]),
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(50))),
-                                            focusedBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: Colors.green[300]),
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(50))),
-                                            prefixIcon: Icon(Icons.lock),
-                                            hintText: 'Password'),
-                                        style: TextStyle(
-                                            fontSize: 20.0,
-                                            color: Colors.black),
-                                        obscureText: true,
-                                      )),
-                                ),
-                              )
-                            ],
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          //login button
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Expanded(
-                                child: Padding(
-                                  padding: EdgeInsets.only(
-                                      right: 100, left: 100, top: 10),
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    height: 60.0,
-                                    decoration: BoxDecoration(
-                                        gradient: LinearGradient(
-                                          begin: Alignment.topRight,
-                                          end: Alignment.bottomLeft,
-                                          colors: [
-                                            Color(0xFF91b3fa),
-                                            Color(0xFF4f52ff)
-                                          ],
-                                        ),
-                                        borderRadius:
-                                            BorderRadius.circular(50)),
-                                    child: FlatButton(
-                                      child: Text("Login",
-                                          style: TextStyle(
-                                              fontSize: 20.0,
-                                              color: Colors.white)),
-                                      onPressed: () async {
-                                        if (_formKey.currentState.validate()) {
-                                          _formKey.currentState.save();
-                                          try {
-                                            await _auth.signIn(
-                                                _email, _password);
-                                            Navigator.pushReplacementNamed(
-                                              context,
-                                              SplashScreen.routeName,
-                                            );
-                                          } catch (e) {
-                                            ScaffoldMessenger.of(context)
-                                                .showSnackBar(
-                                              SnackBar(
-                                                content: Text(e.message),
-                                              ),
-                                            );
-                                          }
-                                        }
-                                      },
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          //sign up navigation
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Expanded(
-                                child: Container(
-                                  alignment: Alignment.center,
-                                  height: 60.0,
-                                  child: GestureDetector(
-                                      child: RichText(
-                                    text: TextSpan(
-                                      text: "Don't have an account? ",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  )),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Center(
-                            child: Column(
-                              children: [
-                                Container(
-                                  decoration: BoxDecoration(
-                                      gradient: LinearGradient(
-                                        begin: Alignment.topRight,
-                                        end: Alignment.bottomLeft,
-                                        colors: [
-                                          Color(0xFF91b3fa),
-                                          Color(0xFF4f52ff)
-                                        ],
-                                      ),
-                                      borderRadius: BorderRadius.circular(50)),
-                                  child: FlatButton(
-                                    onPressed: () {
-                                      Navigator.of(context)
-                                          .pushNamed(SignUpScreen.routeName);
-                                    },
-                                    child: Text(
-                                      "Sign Up",
-                                      style: TextStyle(
-                                        fontSize: 15.0,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(50),
+                  child: Image(
+                    image: AssetImage("assets/covidlogo.png"),
+                    height: 175,
+                  ),
+                ),
+                //Email textfield
+                TextFormField(
+                  onSaved: (value) {
+                    _email = value;
+                  },
+                  controller: emailController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[500]),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50),
+                        ),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(left: 8),
+                        child: Icon(Icons.email, size: 18),
+                      ),
+                      hintText: 'Email'),
+                  style: TextStyle(fontSize: 18.0, color: Colors.black),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  onSaved: (value) {
+                    _password = value;
+                  },
+                  controller: passwordController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[500]),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50),
+                        ),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(left: 8),
+                        child: Icon(Icons.lock, size: 18),
+                      ),
+                      hintText: 'Password'),
+                  style: TextStyle(fontSize: 18.0, color: Colors.black),
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [Color(0xFF91b3fa), Color(0xFF4f52ff)],
+                      ),
+                      borderRadius: BorderRadius.circular(50)),
+                  // ignore: deprecated_member_use
+                  child: FlatButton(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
                       ),
                     ),
-                  ],
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+                        try {
+                          await _auth.signIn(_email, _password);
+                          Navigator.pushReplacementNamed(
+                            context,
+                            SplashScreen.routeName,
+                          );
+                        } catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(e.message),
+                            ),
+                          );
+                        }
+                      }
+                    },
+                  ),
                 ),
-              ),
-            ],
+                //sign up navigation
+                Container(
+                  alignment: Alignment.center,
+                  height: 60.0,
+                  child: GestureDetector(
+                      child: RichText(
+                    text: TextSpan(
+                      text: "Don't have an account? ",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [Color(0xFF91b3fa), Color(0xFF4f52ff)],
+                      ),
+                      borderRadius: BorderRadius.circular(50)),
+                  // ignore: deprecated_member_use
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(SignUpScreen.routeName);
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
