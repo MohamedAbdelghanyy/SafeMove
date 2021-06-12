2ed8da1c41b64d01f04b549a3d7137c6463e25e2
Mohamed Abdelghany <mohamedabdelghanyy@gmail.com>
2021-06-12 02:58:16 +0200

    Update fingerprint_model.dart

--- lib/models/fingerprint_model.dart
+++ lib/models/fingerprint_model.dart
@@ -10,9 +10,9 @@ class FingerprintModel {
     for (int rangeExpander = 0;
         rangeExpander < maxRangeExpanger;
         rangeExpander++) {
-      print('finding');
+      print('Searching for grid...');
+      print('Expanded Range is set to ' + rangeExpander.toString());
       for (int i = 0; i < DataManager.gridsData.length; i++) {
-        print('loop: ' + i.toString());
         bool foundL1 = false, foundL2 = false, foundL3 = false;
         bool foundR1 = false, foundR2 = false, foundR3 = false;
         print(scanResult.length);
@@ -20,62 +20,62 @@ class FingerprintModel {
           print(wifiNetwork.bssid + wifiNetwork.level.toString());
           if (DataManager.gridsData[i].fingerPrintL1[0] == wifiNetwork.bssid &&
               !foundL1) {
-            print('bssid L1');
+            print('Found bssid L1');
             if (DataManager.gridsData[i].fingerPrintL1[1] - rangeExpander <=
                     wifiNetwork.level &&
                 DataManager.gridsData[i].fingerPrintL1[1] + rangeExpander >=
                     wifiNetwork.level) {
               foundL1 = true;
-              print('foundL1');
+              print('Found Fingerprint L1');
             }
           }
           if (DataManager.gridsData[i].fingerPrintL2[0] == wifiNetwork.bssid &&
               !foundL2) {
-            print('bssid L2');
+            print('Found bssid L2');
             if (DataManager.gridsData[i].fingerPrintL2[1] - rangeExpander <=
                     wifiNetwork.level &&
                 DataManager.gridsData[i].fingerPrintL2[1] + rangeExpander >=
                     wifiNetwork.level) {
               foundL2 = true;
-              print('foundL2');
+              print('Found Fingerprint L2');
             }
           }
           if (DataManager.gridsData[i].fingerPrintL3[0] == wifiNetwork.bssid &&
               !foundL3) {
-            print('bssid L3');
+            print('Found bssid L3');
             if (DataManager.gridsData[i].fingerPrintL3[1] - rangeExpander <=
                     wifiNetwork.level &&
                 DataManager.gridsData[i].fingerPrintL3[1] + rangeExpander >=
                     wifiNetwork.level) {
               foundL3 = true;
-              print('foundL3');
+              print('Found Fingerprint L3');
             }
           }
           if (DataManager.gridsData[i].fingerPrintR1[0] == wifiNetwork.bssid &&
               !foundR1) {
-            print('bssid R1');
+            print('Found bssid R1');
             if (DataManager.gridsData[i].fingerPrintR1[1] - rangeExpander <=
                     wifiNetwork.level &&
                 DataManager.gridsData[i].fingerPrintR1[1] + rangeExpander >=
                     wifiNetwork.level) {
               foundR1 = true;
-              print('foundR1');
+              print('Found Fingerprint R1');
             }
           }
           if (DataManager.gridsData[i].fingerPrintR2[0] == wifiNetwork.bssid &&
               !foundR2) {
-            print('bssid R2');
+            print('Found bssid R2');
             if (DataManager.gridsData[i].fingerPrintR2[1] - rangeExpander <=
                     wifiNetwork.level &&
                 DataManager.gridsData[i].fingerPrintR2[1] + rangeExpander >=
                     wifiNetwork.level) {
               foundR2 = true;
-              print('foundR2');
+              print('Found Fingerprint R2');
             }
           }
           if (DataManager.gridsData[i].fingerPrintR3[0] == wifiNetwork.bssid &&
               !foundR3) {
-            print('bssid R3 |' +
+            print('Found bssid R3 |' +
                 (DataManager.gridsData[i].fingerPrintR3[1] - rangeExpander)
                     .toString());
             if (DataManager.gridsData[i].fingerPrintR3[1] - rangeExpander <=
@@ -83,7 +83,7 @@ class FingerprintModel {
                 DataManager.gridsData[i].fingerPrintR3[1] + rangeExpander >=
                     wifiNetwork.level) {
               foundR3 = true;
-              print('foundR3');
+              print('Found Fingerprint R3');
             }
           }
         });
