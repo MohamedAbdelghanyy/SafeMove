90662433882e79e3fb28fa87aa03d334d7a5ccaa
Mohamed Abdelghany <mohamedabdelghanyy@gmail.com>
2021-07-13 17:17:32 +0200

    Update data_manager.dart

--- lib/services/data_manager.dart
+++ lib/services/data_manager.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:safemove/data/global.dart';
 import 'package:safemove/models/grid_model.dart';
 import 'package:safemove/services/firebase_manager.dart';
@@ -17,6 +19,31 @@ class DataManager {
     for (int i = 0; i < Global.grids.length; i++) {
       DataManager.gridsData.add(GridModel.fromMap(Global.grids[i]));
     }
+  }
+
+  static Future<void> getCrowdingData() async {
+    Map<String, dynamic> crowdingData = await FirebaseManager.getCrowdingData();
+    crowdingData.forEach((gridId, data) {
+      for (int i = 0; i < DataManager.gridsData.length; i++) {
+        if (DataManager.gridsData[i].id.toString() == gridId) {
+          int currCrowding = 0;
+          var data2 = data as Map<String, dynamic>;
+          data2.forEach((userId, timeStamp) {
+            Duration diff = DateTime.now().difference(
+                DateTime.fromMillisecondsSinceEpoch(
+                    (int.parse(timeStamp.toString()))));
+            if (diff.inMinutes < 5) {
+              currCrowding++;
+              print(userId.toString() +
+                  ' - Last Active: ' +
+                  diff.inMinutes.toString() +
+                  ' mins. ago');
+            }
+          });
+          DataManager.gridsData[i].crowding = currCrowding;
+        }
+      }
+    });
   }
 
   static Future<void> setSelfReportData(int selfReportStatus) async {
