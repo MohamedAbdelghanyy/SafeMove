d5b2c5577c4528b37673689f9c7a2549d0900112
Mohamed Abdelghany <mohamedabdelghanyy@gmail.com>
2021-07-13 17:22:06 +0200

    Update data_manager.dart

--- lib/services/data_manager.dart
+++ lib/services/data_manager.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:safemove/data/global.dart';
 import 'package:safemove/models/grid_model.dart';
 import 'package:safemove/services/firebase_manager.dart';
@@ -8,8 +6,8 @@ import 'package:safemove/services/perference_manager.dart';
 
 class DataManager {
   static PrefManager mPrefManager = new PrefManager();
-  static List roomsData = new List();
-  static List<GridModel> gridsData = new List();
+  static List roomsData = [];
+  static List<GridModel> gridsData = [];
 
   static Future<void> getRoomsData() async {
     DataManager.roomsData = await FirebaseManager.getRoomsData();
