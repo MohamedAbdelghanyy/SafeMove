0de03aeb8bd14469564cd21e9d8b0f40c2355b4e
Mohamed Abdelghany <mohamedabdelghanyy@gmail.com>
2021-07-14 10:13:29 +0200

    Revert "Update auth.dart"
    This reverts commit 12e9b36501aaca8453e1ba7b25c3c21ae311547a.

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,53 +1,52 @@
-import 'dart:convert';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:safemove/services/data_manager.dart';
-import 'package:http/http.dart' as http;
 
 class Auth {
   final _auth = FirebaseAuth.instance;
   // ignore: unused_field
   final Future<FirebaseApp> _future = Firebase.initializeApp();
-  static const url =
-      'https://safe-move-da6ea-default-rtdb.firebaseio.com/users.json';
+  final databaseRef = FirebaseDatabase.instance.reference().child('users');
 
   Future signUp(name, phone, email, password) async {
     final authresult = await _auth.createUserWithEmailAndPassword(
         email: email, password: password);
-    final response = await http.patch(
-      Uri.parse(url),
-      body: jsonEncode(
-        {
-          _auth.currentUser.uid: {
-            'name': name.toString(),
-            'phone': phone.toString(),
-            'email': email.toString(),
-            'type': 'user',
-          }
-        },
-      ),
-    );
+    databaseRef.child(_auth.currentUser.uid).set({
+      'name': name.toString(),
+      'phone': phone.toString(),
+      'email': email.toString(),
+      'type': 'user',
+    });
     return authresult;
   }
 
   Future signIn(String email, String password) async {
-    try {
-      final authresult = await _auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      final response =
-          await http.get(Uri.parse(url + _auth.currentUser.uid + '.json'));
-      var dbData = json.decode(response.body) as Map<String, dynamic>;
-      var name = dbData['name'];
-      var phone = dbData['phone'];
-      var type = dbData['type'];
-
-      await DataManager.mPrefManager.setLoggedInData(
-          _auth.currentUser.uid, name, phone, email, password, type);
-
-      return authresult;
-    } catch (e) {
-      print('Error' + e.toString());
-    }
+    final authresult = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    var name = (await FirebaseDatabase.instance
+            .reference()
+            .child('users')
+            .child(_auth.currentUser.uid)
+            .child("name")
+            .once())
+        .value;
+    var phone = (await FirebaseDatabase.instance
+            .reference()
+            .child('users')
+            .child(_auth.currentUser.uid)
+            .child("phone")
+            .once())
+        .value;
+    var type = (await FirebaseDatabase.instance
+            .reference()
+            .child('users')
+            .child(_auth.currentUser.uid)
+            .child("type")
+            .once())
+        .value;
+    await DataManager.mPrefManager.setLoggedInData(
+        _auth.currentUser.uid, name, phone, email, password, type);
+    return authresult;
   }
 }
